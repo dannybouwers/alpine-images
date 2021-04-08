75e133f6ff9ed7440a1c9ff362e7616d5546e5ec
Danny Bouwers <d.bouwers+github@gmail.com>
2021-04-08 22:48:59 +0200

    Fixed SSH root login

--- bootstrap/users.sh
+++ bootstrap/users.sh
@@ -8,4 +8,4 @@ done
 
 # enable root ssh
 apk add --no-cache openssh
-sed -i 's|^PermitRootLogin no$|PermitRootLogin yes|g' /etc/ssh/sshd_config
+sed -i 's|^PermitRootLogin .*$|PermitRootLogin yes|g' /etc/ssh/sshd_config
